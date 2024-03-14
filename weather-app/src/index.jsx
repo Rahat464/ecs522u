1e44739d1a6112dae8fada6a7e92212eb42bc067
Rahat Ali <ali.rahat6464@gmail.com>
2024-03-14 19:56:45 +0000

    Removed unnecessary files Fixed CSS for HomeScreen

--- weather-app/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOMClient from "react-dom/client";
-import { HomeScreen } from "./screens/HomeScreen";
-
-const app = document.getElementById("app");
-const root = ReactDOMClient.createRoot(app);
-root.render(<HomeScreen />);
