0e4191037d3a91f5415252b81a70004b0b676ceb
Rahat Ali <ali.rahat6464@gmail.com>
2024-03-20 16:12:18 +0000

    Fix misplaced search button

--- weather-app/src/screens/HomeScreen/index.js
+++ weather-app/src/screens/HomeScreen/index.js
@@ -1,8 +1,8 @@
 export const fetchData = async (userInput) => {
     // Constants
     const API_KEY = {
-        "openweathermap": "74ee18ca0b3d0db25a9331a5899b0a85",
-        "weatherbit": "af697910398e456a84fa8a5943ba6a7f"
+        "openweathermap": "!74ee18ca0b3d0db25a9331a5899b0a85",
+        "weatherbit": "!af697910398e456a84fa8a5943ba6a7f"
     }
     const WEATHER_API_URL = ('https://api.openweathermap.org/data/2.5/forecast?lat={1}&lon={2}&units=metric&appid=' +
         API_KEY["openweathermap"]);
